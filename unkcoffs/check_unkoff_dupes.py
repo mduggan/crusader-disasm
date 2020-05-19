e3d8c542a03cfeac087e1a31984cb2161ddf2bcf
Matthew Duggan <mgithub@guarana.org>
2020-05-19 16:09:42 +0900

    More intrinsic twiddling

--- unkcoffs/check_unkoff_dupes.py
+++ unkcoffs/check_unkoff_dupes.py
@@ -1,19 +1,36 @@
 from struct import unpack
 from collections import defaultdict
-f = open("UNKCOFF.DAT", "rb")
 
-vals = defaultdict(list)
-i = 0
-offsets = f.read()
-ioff = unpack('i' * 311, offsets)
-for i in range(311):
-    ostr = '%08x' % ioff[i]
-    vals[ostr].append(i)
+def print_dupes(ostrs):
+    vals = defaultdict(list)
+    for i, ostr in enumerate(ostrs):
+        vals[ostr].append(i)
+    lists = list(vals.values())
+    lists.sort()
+    for l in lists:
+        if len(l) > 1:
+            print(", ".join('%03X' % x for x in l))
 
-lists = list(vals.values())
 
-lists.sort()
+def print_compare(vals):
+    from remorse_ints import intrinsics
+    #from u8_ints import intrinsics
+    for v, i in zip(vals, intrinsics):
+            print("%s: %s" % (v, i))
 
-for l in lists:
-    if len(l) > 1:
-        print(", ".join('%03X' % x for x in l))
+
+def load_vals():
+    f = open("remorse_UNKCOFF.DAT", "rb")
+    offsets = f.read()
+    nints = len(offsets)/4
+    ioff = unpack('i' * nints, offsets)
+    ostrs = ['%08x' % x for x in ioff]
+    return ostrs
+
+
+def main():
+    vals = load_vals()
+    #print_dupes(vals)
+    print_compare(vals)
+
+main()
