79fc4cd03159bffa8e4da2959226861c07e055f7
Matthew Duggan <mgithub@guarana.org>
2020-06-12 14:22:33 +0900

    A simple script so I don't have to play too much with diff and patch

---
+++ update_disasm_comments.py
@@ -0,0 +1,22 @@
+# A fairly simple script to import my comments from an old disassembly when I
+# update intrinsic names and regenerate the disassembly.
+
+f1 = open('crusader_disasm.txt')
+f2 = open('crusader_disasm_updated.txt')
+
+lines1 = [line for line in f1]
+lines2 = [line for line in f2]
+
+f1.close()
+f2.close()
+
+f3 = open('crusader_disasm_combined.txt', 'w')
+
+for (l1, l2) in zip(lines1, lines2):
+    if l1 == l2:
+        f3.write(l1)
+    elif 'calli' in l1:
+        f3.write(l2)
+    else:
+        f3.write(l1)
+f3.close()
